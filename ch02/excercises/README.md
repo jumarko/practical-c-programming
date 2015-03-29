d0177be726032e91206e6e60c3cc879e69dc404c
Juraj Martinka <juraj.martinka@gooddata.com>
2015-03-29 09:19:45 +0200

    Examples for chapter 2

---
+++ ch02/excercises/README.md
@@ -0,0 +1,7 @@
+# Various programming excercises from different sources
+
+* http://www.c-for-dummies.com/begc4d/exercises/
+* http://en.wikibooks.org/wiki/C_Programming/Beginning_exercises
+* check http://c.learncodethehardway.org/ as well
+* http://www.dummies.com/how-to/content/10-programming-with-c-coding-exercises.html
+
---
+++ ch02/hello/hello.c
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.c
+ *
+ *    Description:  Hello world
+ *
+ *        Version:  1.0
+ *        Created:  03/29/2015 08:54:30
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Juraj Martinka (jumar), jumarko@gmail.com
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+  printf("Hello World\n");
+  return (0);
+}
